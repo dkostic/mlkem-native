067e31711969f5af1fcf2e46f7506e8aca38992c
dkostic <dkostic@amazon.com>
2025-08-19 16:04:41 -0700

    change 4
    Signed-off-by: dkostic <dkostic@amazon.com>

--- dev/x86_64/src/arith_native_x86_64.h
+++ dev/x86_64/src/arith_native_x86_64.h
@@ -76,7 +76,7 @@ void mlk_tomont_avx2(__m256i *r, const int16_t *mlk_qdata);
 void mlk_poly_compress_d4_avx2(uint8_t r[MLKEM_POLYCOMPRESSEDBYTES_D4],
                                const __m256i *MLK_RESTRICT a);
 #define mlk_poly_decompress_d4_avx2 MLK_NAMESPACE(poly_decompress_d4_avx2)
-void mlk_poly_decompress_d4_avx2(__m256i *MLK_RESTRICT r,
+void mlk_poly_decompress_d4_avx2(int16_t *MLK_RESTRICT r,
                                  const uint8_t a[MLKEM_POLYCOMPRESSEDBYTES_D4]);
 #define mlk_poly_compress_d10_avx2 MLK_NAMESPACE(poly_compress10_avx2)
 void mlk_poly_compress_d10_avx2(uint8_t r[MLKEM_POLYCOMPRESSEDBYTES_D10],
